e93accbd57edef8a4172e4e837b0d3eff0520551
Abhigyan <abhigyanmahanta001@gmail.com>
2020-10-16 18:40:50 +0530

    create comment model and association

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,5 @@ class User < ApplicationRecord
     validates :username, presence: true, length: { minimum: 2 }, uniqueness: true
     validates :email, uniqueness: true
     has_many :posts
+    has_many :comments
 end
